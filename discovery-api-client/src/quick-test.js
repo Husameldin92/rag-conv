7981bfc624308817040c44bd92d7659051ad8179
Hossam Osman <hosman@sandsmedia.com>
2026-03-18 10:00:16 +0100

    added notes into the quick-test

--- discovery-api-client/src/quick-test.js
+++ discovery-api-client/src/quick-test.js
@@ -1,3 +1,7 @@
+// this script extract the answer from the stream URL and save it to a CSV file
+// the purpose is to compare discovery and discoveryTest answers for a single question
+
+
 import fetch from 'node-fetch';
 import fs from 'fs';
 import path from 'path';
